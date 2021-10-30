0988cfb9ef56a540b0c606757c28ceea2e09291b
frc <rongcheng_feng@163.com>
2021-10-30 18:09:33 +0800

    使用共用temp array，减少数组空间开辟，进行性能优化

--- src/sort/mergesort/MergeSort.java
+++ src/sort/mergesort/MergeSort.java
@@ -2,7 +2,6 @@ package sort.mergesort;
 
 import linearsearch.ArrayGenerator;
 import sort.SortingHelper;
-import sort.insertionsort.InsertionSort;
 
 import java.util.Arrays;
 
@@ -13,11 +12,11 @@ public class MergeSort {
     }
 
     public static <E extends Comparable<E>> void sort(E[] arr) {
-
-        mergeSort(arr, 0, arr.length - 1);
+        E[] temp = Arrays.copyOf(arr, arr.length);
+        mergeSort(arr, 0, arr.length - 1, temp);
     }
 
-    private static <E extends Comparable<E>> void mergeSort(E[] arr, int l, int r) {
+    private static <E extends Comparable<E>> void mergeSort(E[] arr, int l, int r, E[] temp) {
         if (l >= r) return;
 //        if (r - l <= 15) {
 //            //小规模的数据使用 O(n) 插入排序 优化
@@ -25,31 +24,31 @@ public class MergeSort {
 //            return;
 //        }
         int mid = l + (r - l) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
+        mergeSort(arr, l, mid, temp);
+        mergeSort(arr, mid + 1, r, temp);
         if (arr[mid].compareTo(arr[mid + 1]) > 0) {
-            merge(arr, l, mid, r);
+            merge(arr, l, mid, r, temp);
         }
     }
 
     /**
      * merge arr[l,mid) arr[mid+1,r]
      */
-    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r) {
-        E[] temp = Arrays.copyOfRange(arr, l, r + 1);
+    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r, E[] temp) {
+        System.arraycopy(arr, l, temp, l, r - l + 1);
         int i = l, j = mid + 1;
         for (int k = l; k <= r; k++) {
             if (i > mid) {
-                arr[k] = temp[j - l];
+                arr[k] = temp[j];
                 j++;
             } else if (j > r) {
-                arr[k] = temp[i - l];
+                arr[k] = temp[i];
                 i++;
-            } else if (temp[i - l].compareTo(temp[j - l]) < 0) {
-                arr[k] = temp[i - l];
+            } else if (temp[i].compareTo(temp[j]) < 0) {
+                arr[k] = temp[i];
                 i++;
             } else {
-                arr[k] = temp[j - l];
+                arr[k] = temp[j];
                 j++;
             }
         }
